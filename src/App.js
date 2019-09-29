124dddf5c31d25aed1007df0d823b8f861bc9948
James Hooper <jameshooperdesigns@gmail.com>
2019-09-29 15:04:53 -0600

    Add components to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
 function App() {
   return (
     <div className="App">
       <h1>Luxury Hotel</h1>
+      <Home></Home>
+      <Rooms></Rooms>
+      <SingleRoom></SingleRoom>
+      <Error></Error>
     </div>
   );
 }
