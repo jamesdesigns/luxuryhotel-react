d4fcec2d0371b37a4f7c8bb650f9907f6d3ca62b
James Hooper <jameshooperdesigns@gmail.com>
2019-10-08 22:55:27 -0600

    Add 404 error page to the website

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,14 @@ import Banner from '../components/Banner';
 import {Link} from 'react-router-dom';
 
 export default function Home() {
-    return <Hero>
+    return (
+    <Hero>
         <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
             <Link to='/rooms' className="btn-primary">
                 our rooms
             </Link>
         </Banner>
-    </Hero>;
+    </Hero>
+    );
 }
 
