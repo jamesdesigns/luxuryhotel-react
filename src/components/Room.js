ff81311122602d2ca287d9bd0d0335548777a4ce
James Hooper <jameshooperdesigns@gmail.com>
2019-10-17 17:06:21 -0600

    Add an alt tag to the img src

--- src/components/Room.js
+++ src/components/Room.js
@@ -6,7 +6,7 @@ export default function Room({room}) {
 
     return <article className="room">
                 <div className="img-container">
-                <img src={images[0]} alt=""/>
+                <img src={images[0]} alt="single room"/>
                 </div>
             </article>;
 }
