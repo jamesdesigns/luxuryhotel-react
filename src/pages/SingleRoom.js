3247fb2791dcaa418a3f38eea00703317635ba49
James Hooper <jameshooperdesigns@gmail.com>
2019-10-20 12:14:09 -0600

    Add ternary operator on line 71

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -67,8 +67,10 @@ export default class SingleRoom extends Component {
                     <h3>info</h3>
                     <h6>price : ${price}</h6>
                     <h6>size : {size} SQFT</h6>
-                    <h6>Max Capacity : {capacity} People</h6>
-                    <h6>{}</h6>
+                    <h6>max capacity :{" "}
+                     {capacity > 1 ? `${capacity} people` : `${capacity} person` }
+                    </h6>
+                    <h6></h6>
                 </article>
 
             </div>
