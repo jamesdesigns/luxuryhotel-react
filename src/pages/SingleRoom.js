7f4e3ad12827161546f8af0e312352d7f4a70853
James Hooper <jameshooperdesigns@gmail.com>
2019-10-19 19:00:39 -0600

    Add description and price to component

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -59,7 +59,15 @@ export default class SingleRoom extends Component {
                 })}
             </div>
             <div className="single-room-info">
-                
+                <article className="desc">
+                    <h3>details</h3>
+                    <p>{description}</p>
+                </article>
+                <article className="info">
+                    <h3>info</h3>
+                    <h6>price : ${price}</h6>
+                </article>
+
             </div>
         </section>
         </>
