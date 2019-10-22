713799847569e5867f138fa43c9f339c89115647
James Hooper <jameshooperdesigns@gmail.com>
2019-10-22 15:14:04 -0600

    Begin building out the search form

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {useContext} from 'react';
+import {RoomContext} from '../context';
+import Title from '../components/Title';
 
 export default function RoomFilter() {
+    const context = useContext(RoomContext);
+    const {
+        handleChange,type,capacity,price,minPrice,maxPrice,minSize,maxSize,breakfast,pets
+    } = context;
+
     return (
-        <div>
-            hello from room filter
-        </div>
+        <section className="filter-container">
+            <Title title="search rooms" />
+            <form className="filter-form">
+                {/* select type */}
+                {/* end select  */}
+            </form>
+            
+        </section>
     )
 }
