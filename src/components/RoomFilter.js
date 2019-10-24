fad0bc52969484af4a385dd49c1d2dec45b4163d
James Hooper <jameshooperdesigns@gmail.com>
2019-10-24 17:32:40 -0600

    Add to the Select tag in the RoomFilter component

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -2,12 +2,23 @@ import React from 'react';
 import {useContext} from 'react';
 import {RoomContext} from '../context';
 import Title from '../components/Title';
-
-export default function RoomFilter() {
+// get all unique values
+const getUnique = (items,value) => {
+    return [...new Set(item.map(item => item[value]))]
+}
+export default function RoomFilter({rooms}) {
     const context = useContext(RoomContext);
     const {
         handleChange,type,capacity,price,minPrice,maxPrice,minSize,maxSize,breakfast,pets
     } = context;
+    // get unique types
+    let types = getUnique(rooms,'type');
+    // add all
+    types = ['all',...types];
+    // map to jsx
+    types = types.map((item,index)=>{
+        return <option value={item} key={index}>{item}</option> 
+    })
 
     return (
         <section className="filter-container">
@@ -16,7 +27,16 @@ export default function RoomFilter() {
                 {/* select type */}
                 <div className="form-group">
                     <label htmlFor="type">room type</label>
-                    <select name="type" id="type" value={type} className="form-control" onChange={handleChange}></select> 
+                    <select 
+                    name="type" 
+                    id="type" 
+                    value={type} 
+                    className="form-control" 
+                    onChange={handleChange}
+                    >
+                        {types}
+                    </select> 
+                    <option></option>
                 </div>
                 {/* end select  */}
             </form>
